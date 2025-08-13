fa88c0a84ef05818b61660777bb350806ea907a9
confidence <nsirimconfidence@yahoo.com>
2025-08-13 12:57:58 +0100

    removed the local image storage middleware

--- routers/productRouter.js
+++ routers/productRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 const { identifier } = require('../middleware/identification'); // Your auth middleware
 const { authorizeRoles } = require('../middleware/authorization'); // Authorization middleware
-const upload = require("../middleware/upload");
+// const upload = require("../middleware/upload");
 const upload = require('../cloudinaryConfig');
 
 

de5a1c9e268d9158cbcf23eb09b60d34ed4074ba
confidence <nsirimconfidence@yahoo.com>
2025-08-14 13:20:37 +0100

    Add CORS configuration

--- server.js
+++ server.js
@@ -6,15 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Sample route
-app.get('/api/products', (req, res) => {
-  res.json([
-    { id: 1, name: 'Sneakers', price: 59.99 },
-    { id: 2, name: 'Shirt', price: 29.99 },
-    { id: 3, name: 'Pants', price: 39.99 },
-    
-  ]);
-});
+
 
 app.get("/api/index", (_, response) => {
   console.log('Root route accessed!'); // <--- ADD THIS LINE TEMPORARILY
